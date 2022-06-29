539700306ba1d322780caf67c144907d7aeee6b7
milksake <caceresmariana443@gmail.com>
2022-06-29 18:26:42 -0500

    ej3 fixed :))

--- ej3.py
+++ ej3.py
@@ -2,11 +2,12 @@ from hashlib import sha1
 from RSA import rsaKeyGenerator, rsaCipher
 
 if __name__ == "__main__":
-    (P, S) = rsaKeyGenerator(32)
+    (P, S) = rsaKeyGenerator(162)
     M = b"HelloWorld"
     h = sha1()
     h.update(M)
     m = int(h.hexdigest(), 16)
+    print(h.hexdigest())
 
     # Firma digital
     sigma = rsaCipher(m, S)
@@ -14,5 +15,3 @@ if __name__ == "__main__":
     u = rsaCipher(sigma, P)
 
     print(M, m, sigma, u, u == m)
-
-    
