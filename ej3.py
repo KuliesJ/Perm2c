8471f828ed3deb34993b4cb9a3270688647e9b5a
KuliesJ <jorge.nunez.paucar@ucsp.edu.pe>
2022-07-02 18:05:05 -0500

    ej3 fixed

--- ej3.py
+++ ej3.py
@@ -2,13 +2,13 @@ from hashlib import sha1
 from RSA import rsaKeyGenerator, rsaCipher
 
 if __name__ == "__main__":
-    (P, S) = rsaKeyGenerator(162)
+    (P, S) = rsaKeyGenerator(32)
     M = b"HelloWorld"
     h = sha1()
     h.update(M)
     m = int(h.hexdigest(), 16)
     print(h.hexdigest())
-
+    m = m%P[1]
     # Firma digital
     sigma = rsaCipher(m, S)
 
